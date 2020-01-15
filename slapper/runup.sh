fa49527cd532fb81a85435f87e02ba6345f15b8c
Pascal Andy <pascal@firepress.org>
2020-01-15 10:06:29 -0500

    add slapper /b

--- slapper/runup.sh
+++ slapper/runup.sh
@@ -1,2 +1,6 @@
+docker run ctr.run/github.com/firepress-org/dockerfile-utilities/slapper:master
+
+
+
 docker run ctr.run/github.com/firepress-org/dockerfile-utilities/slapper:master sh -c \
     "htpasswd -nbB ${USER} ${PW}" | sed -e s/\\$/\\$\\$/g
